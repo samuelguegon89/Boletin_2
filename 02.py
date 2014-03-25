afbdfb117f1e6bd7549d288e02a32e872e27e63b
samuelguegon89 <samuelguegongithub@gmail.com>
2014-03-25 15:58:12 +0100

    Segundo ejercicio del Boletin 2 resuelto

---
+++ 02.py
@@ -0,0 +1,14 @@
+from random import random
+#round(numero,3)
+#lista = []
+#print '%.3f '% round(lista[i],3)
+#lista.append(random.random())
+#prueb1=round(prueb,3)
+
+numero=int(raw_input("Dame un numero: "))
+lista = []
+
+
+for i in range(numero):
+	lista.append(random())
+	print "la lista de",numero,""
